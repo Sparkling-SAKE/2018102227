a6cb8c69ed28b3b61cdda47070c43dd888d8b206
JeonHyeonJin <co22oc@khu.ac.kr>
2021-04-09 10:15:09 +0900

    MulTable to 8

---
+++ MulTable/main.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+using namespace std;
+
+const int RANGE = 8;
+
+int main()
+{
+	for (int i = 1; i < RANGE + 1; ++i)
+		for (int j = 1; j < RANGE + 1; ++j)
+			cout << i << '*' << j << '=' << i * j << endl;
+
+	return 0;
+}
